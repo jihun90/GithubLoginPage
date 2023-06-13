43ae7011628a30576a273b32d6a107568fb2d233
jihun90 <jihun90.kim@icloud.com>
2023-06-13 21:26:53 +0900

    코드 정리

--- src/routes/GitHubInfo.tsx
+++ src/routes/GitHubInfo.tsx
@@ -6,28 +6,42 @@ import { useEffect } from "react";
 const GITHUB_CLIENT_ID: string = import.meta.env.VITE_GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRETS: string = import.meta.env.VITE_GITHUB_CLIENT_SECRETS;
 
+interface GithubClientInfo {
+  client_id: string;
+  client_secret: string;
+  code: string | null;
+}
+
+interface GithubHeaders {
+  baseURL: string;
+  headers: {
+    [key: string]: string;
+  };
+  withCredentials: boolean;
+}
+
 function GithubInfo(): JSX.Element {
   const [searchParams]: TypeOfUseSearchParam = useSearchParams();
   const code: string | null = searchParams.get("code");
 
+  const ClientInfo: GithubClientInfo = {
+    client_id: GITHUB_CLIENT_ID,
+    client_secret: GITHUB_CLIENT_SECRETS,
+    code: code,
+  };
+
+  const headers: GithubHeaders = {
+    baseURL: import.meta.env.BASE_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  };
+
   useEffect(() => {
     (async () => {
       await axios
-        .post(
-          import.meta.env.VITE_GITHUB_ACCESS_TOKEN_URL,
-          {
-            client_id: GITHUB_CLIENT_ID,
-            client_secret: GITHUB_CLIENT_SECRETS,
-            code: code,
-          },
-          {
-            baseURL: import.meta.env.BASE_URL,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            withCredentials: true,
-          }
-        )
+        .post(import.meta.env.VITE_GITHUB_ACCESS_TOKEN_URL, ClientInfo, headers)
         .then((respones) => {
           console.log(respones);
           if (respones.status === 200) {
@@ -40,7 +54,7 @@ function GithubInfo(): JSX.Element {
         .catch((response) => console.log("[error]" + response));
     })(),
       [];
-  }, []);
+  });
 
   return <div>Loading...</div>;
 }
